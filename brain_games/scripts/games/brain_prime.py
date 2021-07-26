11f0a91bdec45d3d316621ea8c9ae9be1d362bff
ALezhnin <lezhnin.alex@gmail.com>
2021-07-26 09:12:41 +0300

    #00800 Add: is it prime game

---
+++ brain_games/scripts/games/brain_prime.py
@@ -0,0 +1,33 @@
+"""Is it prime game."""
+from random import SystemRandom
+from math import sqrt
+
+from brain_games.game_processor import game
+
+
+def main():
+    """
+    Run is it prime game.
+
+    Returns:
+        str
+    """
+    explain = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+    def args():
+        max_number = 1000
+        return SystemRandom().randrange(max_number)
+
+    def question(number):
+        return str(number)
+
+    # flake8: noqa: C901
+    def correct_answer(arguments):
+        if arguments < 4:
+            return 'yes'
+        for n in range(2, int(sqrt(arguments))):
+            if arguments % n == 0:
+                return 'no'
+        return 'yes'
+
+    game(explain, args, question, correct_answer)
